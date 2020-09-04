5ea80d6e6c4a2a30d18954640edeeea50ce93644
Kelsey Oshiro <kelseymosh@gmail.com>
2020-09-04 00:58:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,8 +15,11 @@ def load_library(yaml_file)
 end
 
 
-def get_english_meaning()
-
+def get_english_meaning(yaml_file, japanese_emoticon)
+  library = load_library.each do |meaning, emoticons|
+    if library[meaning][:japanese] == japanese_emoticon
+      return library[meaning][:english]
+  end
 end
 
 def get_japanese_emoticon()
