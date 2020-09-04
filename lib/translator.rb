ff7774b04e042923a4ecaf4f3a7727beffe726c7
Kelsey Oshiro <kelseymosh@gmail.com>
2020-09-04 00:50:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,17 +2,18 @@ require 'yaml'
 require 'pry'
 
 def load_library(yaml_file)
-  emoticons = yaml.load_file('./lib/emoticons.yml')
-    final = emoticons.each_with_object ({}) do |(key, value), final_hash)|
+  emoticons = YAML.load_file('./lib/emoticons.yml')
+    final = emoticons.each_with_object ({}) do |(key, value), final_hash|
       if !final_hash[key]
         final_hash[key] = {
-          :english => value[0],
-          :japanese => value[1]
-        }
+        :english => value[0],
+        :japanese => value[1]
+      }
     end
+    final
   end
-  final
 end
+
 
 def get_english_meaning()
 
